a862aff858a4494d9a7e4564b45c333a087990a2
Kārlis <voiconokskarls@gmail.com>
2026-02-11 13:45:18 +0200

    add initial react frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Layout from './layouts/Layout';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Layout>
+  );
+}
+
+export default App;
