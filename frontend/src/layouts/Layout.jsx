dc9c8a3aad4426a9e5d6be68b029995bbc16be65
Kārlis <voiconokskarls@gmail.com>
2026-02-14 15:27:25 +0200

    add initial layout styling

--- frontend/src/layouts/Layout.jsx
+++ frontend/src/layouts/Layout.jsx
@@ -4,15 +4,24 @@ import { Link } from 'react-router-dom';
 const Layout = ({ children }) => {
   return (
     <>
-      <header>
-        <nav className="p-2 border">
+      <header className="max-w-360 mx-auto">
+        <nav className="border border-primary-light">
           <div>
-            <Link to="/">Home</Link>
-            <Link to="/build">Build</Link>
+            <h1 className="p-4 text-3xl font-bold">PC BUILDER</h1>
+          </div>
+
+          <div className="flex items-center space-x-2 bg-linear-to-b from-primary-light to-primary text-white px-6 rounded-lg -mx-2">
+            <Link className="text-sm py-1 px-2 border-r border-primary-light" to="/">
+              Home
+            </Link>
+            <Link className="text-sm py-1" to="/build">
+              Build
+            </Link>
           </div>
         </nav>
       </header>
-      <main>{children}</main>
+
+      <main className="max-w-360 mx-auto border">{children}</main>
     </>
   );
 };
