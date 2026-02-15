cb4f330f9f779875fe282a14338e913b5cb2612f
Kārlis <voiconokskarls@gmail.com>
2026-02-15 16:59:11 +0200

    improve slider

--- frontend/src/components/PriceSlider.jsx
+++ frontend/src/components/PriceSlider.jsx
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
 
 const PriceSlider = ({ setBudget, budget }) => {
-  const handleChange = (e) => {
+  const handleSlider = (e) => {
     setBudget(Number(e.target.value));
   };
 
   return (
-    <div className="flex flex-col w-50">
-      <label className="text-center" htmlFor="range-slider">
-        Price: {budget} €
+    <div className="flex flex-col w-50 items-center">
+      <label className="text-center flex" htmlFor="budget">
+        Price:{' '}
+        <input
+          className="w-15 text-center"
+          type="text"
+          value={budget}
+          id="budget"
+          onChange={(e) => setBudget(e.target.value)}
+        />{' '}
+        €
       </label>
 
       <input
-        className="accent-primary"
-        id="range-slider"
+        className="accent-primary w-60"
         type="range"
         min={500}
         max={5000}
         value={budget}
-        onChange={handleChange}
+        onChange={handleSlider}
       />
     </div>
   );
