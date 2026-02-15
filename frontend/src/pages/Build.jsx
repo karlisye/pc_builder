1cd526af1601550c66da00175342c4294430d3b2
Kārlis <voiconokskarls@gmail.com>
2026-02-15 20:43:35 +0200

    add remove option for builds

--- frontend/src/pages/Build.jsx
+++ frontend/src/pages/Build.jsx
@@ -37,13 +37,18 @@ const Build = () => {
           className="bg-linear-to-b from-success-light to-success-dark text-white font-bold text-base py-3 px-6 rounded-xl hover:cursor-pointer"
           onClick={handleGenerate}
         >
-          Generate
+          {build ? 'Regenerate' : 'Generate'}
         </button>
       </div>
 
-      <div className="flex-2 border flex items-center justify-center">
+      <div className="flex-2 border flex items-center justify-center relative">
         {build ? (
-          <Computer components={build} />
+          <>
+            <Computer components={build} />
+            <button className="absolute top-0 right-0 text-danger" onClick={() => setBuild(null)}>
+              Remove
+            </button>
+          </>
         ) : (
           <div className="text-center">
             <h2 className="text-xl font-semibold">Generate a PC to view the parts</h2>
