b09f1cb1af1f11bd1801bbe4c8a137368005d6ff
Kārlis <voiconokskarls@gmail.com>
2026-02-15 17:14:26 +0200

    add price field validation and input

--- frontend/src/components/PriceSlider.jsx
+++ frontend/src/components/PriceSlider.jsx
@@ -1,23 +1,38 @@
 import React, { useState } from 'react';
 
 const PriceSlider = ({ setBudget, budget }) => {
+  const [error, setError] = useState('');
+
   const handleSlider = (e) => {
     setBudget(Number(e.target.value));
   };
 
+  const handleInput = (e) => {
+    if (e.target.value < 500 || e.target.value > 5000) {
+      console.log('a');
+      setError('Please enter a number between 500 and 5000');
+    } else {
+      setError('');
+    }
+
+    setBudget(e.target.value);
+  };
+
   return (
-    <div className="flex flex-col w-50 items-center">
+    <div className="flex flex-col items-center">
       <label className="text-center flex" htmlFor="budget">
         Price:{' '}
         <input
-          className="w-15 text-center"
-          type="text"
+          className={`w-15 text-center rounded-md ${error ? 'outline-red-500 outline-2' : ''}`}
+          type="number"
           value={budget}
           id="budget"
-          onChange={(e) => setBudget(e.target.value)}
+          onChange={handleInput}
         />{' '}
         €
       </label>
+
+      {error && <p className="text-red-500">{error}</p>}
 
       <input
         className="accent-primary w-60"
