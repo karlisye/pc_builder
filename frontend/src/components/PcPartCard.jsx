0f71f59e6d68df59a6fbad9d261f495be320d59f
Kārlis <voiconokskarls@gmail.com>
2026-02-21 17:12:06 +0200

    improve styling for list view

--- frontend/src/components/PcPartCard.jsx
+++ frontend/src/components/PcPartCard.jsx
@@ -7,25 +7,44 @@ const PcPartCard = ({ title, part }) => {
     setSelectedComponent(part);
     setIsModalActive(true);
   };
+
   return (
-    <div className="bg-linear-to-br from-primary-light to-primary p-2 rounded-md">
-      <h3 className="font-bold text-3xl text-white">{title}</h3>
-      <p className="font-bold text-2xl text-slate-400">{part.name}</p>
-      <div className="flex items-center justify-between">
-        <span className="text-success-light font-bold text-2xl">{part.price}€</span>
-        <div>
-          <button className="text-white hover:text-success-light" onClick={handleSeeMore}>
-            More
-          </button>
-          <a
-            className="text-white hover:text-success-light"
-            href={part.url}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Order Now
-          </a>
-        </div>
+    <div className="bg-primary p-6 rounded-xl border">
+      <div className="flex items-center justify-between mb-4">
+        <h3 className="font-bold text-2xl bg-primary-light backdrop-blur-sm px-4 py-2 rounded-lg text-white">
+          {title}
+        </h3>
+        <span className="px-3 py-1 bg-success-dark/50 text-success-light text-sm font-semibold rounded-full">
+          {part.price}€
+        </span>
+      </div>
+
+      <h4 className="font-bold text-xl text-white mb-4 truncate pr-2">{part.name}</h4>
+
+      {/* Action Buttons */}
+      <div className="flex items-center gap-4 pt-4">
+        <button
+          className="flex-1 bg-primary-light text-white font-semibold py-2 px-4 rounded-lg flex items-center justify-center gap-2 hover:cursor-pointer hover:bg-primary-light/50"
+          onClick={handleSeeMore}
+        >
+          <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+          More Info
+        </button>
+
+        <a
+          className="bg-success-light hover:bg-success-dark text-primary-dark font-semibold py-2 px-6 rounded-lg shadow-md"
+          href={part.url}
+          target="_blank"
+        >
+          Order Now
+        </a>
       </div>
     </div>
   );
