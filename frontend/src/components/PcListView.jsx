713e735c30dfe96d5eac7663d52c2c2a0ab9ce98
Kārlis <voiconokskarls@gmail.com>
2026-02-24 19:37:17 +0200

    add disabled effect in list view

--- frontend/src/components/PcListView.jsx
+++ frontend/src/components/PcListView.jsx
@@ -6,17 +6,17 @@ const PcListView = () => {
   const { build } = useBuild();
 
   return (
-    <div className='bg-primary-light px-3 pt-6 pb-3 rounded-br-lg'>
+    <div className="bg-primary-light px-3 pt-6 pb-3 rounded-br-lg">
       <div className="w-full flex p-4 flex-wrap justify-center bg-primary rounded-lg">
-        {build.motherboard && <PcPartCard title="Motherboard" part={build.motherboard} />}
-        {build.cpu && <PcPartCard title="CPU" part={build.cpu} />}
-        {build.cooler && <PcPartCard title="Cooler" part={build.cooler} />}
-        {build.gpu && <PcPartCard title="Graphics Card" part={build.gpu} />}
-        {build.ram && <PcPartCard title="RAM" part={build.ram} />}
-        {build.psu && <PcPartCard title="Power Supply" part={build.psu} />}
-        {build.ssd && <PcPartCard title="SSD" part={build.ssd} />}
-        {build.case && <PcPartCard title="Case" part={build.case} />}
-        {build.fans && <PcPartCard title="Fans" part={build.fans} />}
+        <PcPartCard title="Motherboard" component={build.motherboard} />
+        <PcPartCard title="CPU" component={build.cpu} />
+        <PcPartCard title="Cooler" component={build.cooler} />
+        <PcPartCard title="Graphics Card" component={build.gpu} />
+        <PcPartCard title="RAM" component={build.ram} />
+        <PcPartCard title="Power Supply" component={build.psu} />
+        <PcPartCard title="SSD" component={build.ssd} />
+        <PcPartCard title="Case" component={build.case} />
+        <PcPartCard title="Fans" component={build.fans} />
       </div>
     </div>
   );
--- frontend/src/components/PcPartCard.jsx
+++ frontend/src/components/PcPartCard.jsx
@@ -1,45 +1,62 @@
 import { useBuild } from '../contexts/BuildContext';
 
-const PcPartCard = ({ title, part }) => {
+const PcPartCard = ({ title, component }) => {
   const { setSelectedComponent, setIsModalActive } = useBuild();
 
   const handleSeeMore = () => {
-    setSelectedComponent(part);
+    setSelectedComponent(component);
     setIsModalActive(true);
   };
 
   return (
-    <div className="lg:w-1/3 md:w-1/2 w-full">
-      <div className='bg-primary-dark p-3 m-2 rounded-xl'>
-        <div className="flex lg:flex-col flex-row items-center lg:items-start justify-between mb-4 gap-2">
-          <h3 className="font-bold text-2xl bg-primary-light backdrop-blur-sm px-4 py-2 rounded-lg text-secondary">
-            {title}
-          </h3>
-          <span className="px-3 py-1 bg-success-dark/50 text-success-light text-sm font-semibold rounded-full">
-            {part.price}€
-          </span>
-        </div>
+    <div className="lg:w-1/3 md:w-1/2 w-full p-2">
+      <div className="bg-primary-dark p-3 rounded-xl h-full">
+        {component ? (
+          <>
+            <div className="flex lg:flex-col flex-row items-center lg:items-start justify-between mb-4 gap-2">
+              <h3 className="font-bold text-2xl bg-primary-light backdrop-blur-sm px-4 py-2 rounded-lg text-secondary">
+                {title}
+              </h3>
+              <span className="px-3 py-1 bg-success-dark/50 text-success-light text-sm font-semibold rounded-full">
+                {component.price}€
+              </span>
+            </div>
 
-        <h4 className="font-bold text-xl text-white truncate" title={part.name}>
-          {part.name}
-        </h4>
+            <h4 className="font-bold text-xl text-white truncate" title={component.name}>
+              {component.name}
+            </h4>
 
-        <div className="flex items-center gap-4 pt-4">
-          <button
-            className="flex-1 bg-primary-light text-white font-semibold py-2 px-4 rounded-lg flex items-center justify-center gap-2 hover:cursor-pointer hover:bg-primary-light/50"
-            onClick={handleSeeMore}
-          >
-            More
-          </button>
+            <div className="flex items-center gap-4 pt-4">
+              <button
+                className="flex-1 bg-primary-light text-white font-semibold py-2 px-4 rounded-lg flex items-center justify-center gap-2 hover:cursor-pointer hover:bg-primary-light/50"
+                onClick={handleSeeMore}
+              >
+                More
+              </button>
 
-          <a
-            className="bg-secondary hover:bg-secondary-dark text-primary-dark font-semibold py-2 px-6 rounded-lg shadow-md"
-            href={part.url}
-            target="_blank"
-          >
-            Order
-          </a>
-        </div>
+              <a
+                className="bg-secondary hover:bg-secondary-dark text-primary-dark font-semibold py-2 px-6 rounded-lg shadow-md"
+                href={component.url}
+                target="_blank"
+              >
+                Order
+              </a>
+            </div>
+          </>
+        ) : (
+          <div className="h-full flex flex-col items-center justify-center gap-4">
+            <h3 className="font-bold text-2xl bg-primary-light backdrop-blur-sm px-4 py-2 rounded-lg text-primary-dark">
+              {title}
+            </h3>
+            <span className="text-sm text-primary-light text-center">
+              {title === 'Graphics Card'
+                ? 'The CPU includes an integrated graphics card'
+                : title === 'Cooler'
+                  ? 'The CPU comes with a cooler'
+                  : ''}
+            </span>
+          </div>
+        )}
       </div>
     </div>
   );
