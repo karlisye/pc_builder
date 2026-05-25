ef67385f825cb786add99234718e42dce0cc8d95
Kārlis <voiconokskarls@gmail.com>
2026-05-25 13:03:00 +0300

    add remote origin to gitlab

--- database/testing/components.sql
+++ database/testing/components.sql
@@ -19,7 +19,7 @@
 -- Table structure for table `cpus`
 --
 
-DROP TABLE IF EXISTS `cpus`;
+DROP TABLE IF EXISTS `cpus`; 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cpus` (
